5e5378d6200ffa05d1cacf8987c953f2ad5c3f47
titouanmoulin <titouan.moulin@etu.unilim.fr>
2024-11-20 11:38:53 +0100

    base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react(), tailwind()],
 });
