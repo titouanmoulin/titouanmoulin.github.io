d514d9363342e5999ec7ece2ace31d7c5b2cbab2
titouanmoulin <titouan.moulin@etu.unilim.fr>
2024-11-21 08:43:24 +0100

    astroconfigbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://titouanmoulin.github.io',
-    base: 'my-repo',
+    // base: 'my-repo',
     integrations: [mdx(), sitemap(), react(), tailwind()],
 });
